8bc11e5bcd10032de91ab907d4b4fadbac907209
Spookfu <spookfu@proton.me>
2025-08-17 01:21:02 -0400

    Inital Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+if(document.URL.includes("index.html")) {
+
+} else if(document.URL.includes("projects.html")) {
+document.querySelector(".nav_item_proj").classList.add('active');
+} else if(document.URL.includes("contacts.html")) {
+document.querySelector(".nav_item_contact").classList.add('active');
+}
