4cdd5f7855f3beaff2632da1c1ae514e4f5d276c
Spookfu <spookfu@proton.me>
2025-09-03 01:08:23 -0400

    Update 09032025-1

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 if(document.URL.includes("index.html")) {
-
+document.querySelector(".nav_item_home").classList.add('active');
+document.querySelector(".nav").classList.add('home');
+document.querySelector(".nav_status").classList.add('home');
 } else if(document.URL.includes("projects.html")) {
 document.querySelector(".nav_item_proj").classList.add('active');
 document.querySelector(".nav").classList.add('project');
-} else if(document.URL.includes("contacts.html")) {
-document.querySelector(".nav_item_contact").classList.add('active');
-document.querySelector(".nav").classList.add('contact');
+document.querySelector(".nav_status").classList.add('project');
 }
